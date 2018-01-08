ec9a8fed4af69513a159f78d730e95a4aa96bbbc
Henrique Craveiro <henrique.cne@gmail.com>
2018-01-08 09:28:19 -0200

    Added request to dependecies

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/api/v1/cotacao', (req, res) => {
+  .get('/api/v1/quotation', (req, res) => {
     request
       .get('https://www.melhorcambio.com/dolar_hoje/cotacao_cidade.php?idmoeda=11&idcidade=2&comercial=3%2C89', (error, response) => {
         res.json(response.body)
