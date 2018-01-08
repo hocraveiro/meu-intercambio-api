383eac759a31038062cbc7a43d6068315da7dba3
Henrique Craveiro <henrique.cne@gmail.com>
2018-01-08 09:21:51 -0200

    API quotation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const request = require('request')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,8 +9,9 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/api/v1/cotacao', (req, res) => {
-    res.json({success: {
-      teste: 1
-    }})
+    request
+      .get('https://www.melhorcambio.com/dolar_hoje/cotacao_cidade.php?idmoeda=11&idcidade=2&comercial=3%2C89', (error, response) => {
+        res.json(response.body)
+      })
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
